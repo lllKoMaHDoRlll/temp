19156e50bebe922c038e9c44f231c617c0df1ef2
lllKoMaHDoRlll <88376471+lllKoMaHDoRlll@users.noreply.github.com>
2023-12-23 21:33:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,14 @@
-function send(event) {
-    // убираем стандартное поведение формы
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+    var mobileMenuIconEl = document.getElementById("mobile-menu-icon");
+    var mobileMenuEl = document.getElementById("mobile-menu-outter");
 
-    // Валидация на чекбокс
-    let acceptionEl = document.getElementById("uacception");
-    if (!acceptionEl.checked) {
-        alert("Примите политику обработки персональных данных");
-        return false;
-    }
-
-    // Создаем новый запрос
-    let request = new XMLHttpRequest();
-    request.open("POST", "https://formcarry.com/s/G9Dc0K8CB1");
-    request.setRequestHeader("ACCEPT", "application/json");
-
-    // Заполняем данные
-
-    let data = new FormData();
-    
-    let inputEls = document.getElementsByClassName("save");
-    [...inputEls].forEach(inputEl => {
-        data.append(inputEl.id, inputEl.value);
-    });
-    
-    // Устанавливаем обработку на изменение состояния запроса
-    request.onreadystatechange = () => {
-        if (request.readyState === XMLHttpRequest.DONE) {
-          const status = request.status;
-          if (status === 0 || (status >= 200 && status < 400)) {
-            alert("Форма успешно отправлена");
-          } else {
-            alert("Произошла ошибка." + status);
-          }
-        }
-    };
-
-    // Отправляем данные
-    request.send(data);
-    // Отчистка хранилища
-    [...inputEls].forEach(inputEl => {
-        inputEl.value = "";
-        localStorage.setItem(inputEl.id, "");
+    mobileMenuIconEl.addEventListener("click", () => {
+        mobileMenuEl.style.display = "block";
     });
 
-    hide();
-}
-
-function show () {
-    // второй параметр устарел, передается пустая строка
-    history.pushState({form: true}, "", "./#form");
-    let modal = document.getElementById("modal");
-    modal.setAttribute("data--modal", "shown");
-}
-
-function hide() {
-    // второй параметр устарел, передается пустая строка
-    history.replaceState({form: false}, "", "./");
-    let modal = document.getElementById("modal");
-    modal.setAttribute("data--modal", "hidden");
-}
-
-function saveInput(event) {
-    localStorage.setItem(event.target.id, event.target.value);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    if (history.state == null) {
-        history.pushState({form: false}, "", "./");
-    }
-    // проверка на активность формы
-    if (history.state.form) {
-        show();
-    }
-    
-    let feedbackButtonEl = document.getElementById("feedback-button");    
-    feedbackButtonEl.addEventListener('click', show);
-
-    let closeButton = document.getElementById("close-button");
-    closeButton.addEventListener("click", hide);
-
-    let inputEls = document.getElementsByClassName("save");
-
-    [...inputEls].forEach(inputEl => {
-        // Загрузка значений в инпуты
-        inputEl.value = localStorage.getItem(inputEl.id);
+    document.addEventListener("click", (event) => {
+        if (event.target != mobileMenuIconEl) {
+            mobileMenuEl.style.display = "none";
+        }  
     });
-    
-    [...inputEls].forEach(inputEl => {
-        // Добавляем ивент на сохранение значения
-        inputEl.addEventListener("input", saveInput);
-    });
-
-    let submitButton = document.getElementById("submit-button");
-    submitButton.addEventListener("click", send);
 });
